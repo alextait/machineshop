6f6142b34ace54da48be3ba7b3dec799aa746306
alextait <alextait@gmail.com>
2017-06-28 12:59:28 +0100

    updated

--- resources/views/project-highlight.blade.php
+++ resources/views/project-highlight.blade.php
@@ -7,7 +7,17 @@
     <div class="swiper-container swiper-container-horizontal swiper-container-fade">
         <div class="swiper-wrapper" style="transition-duration: 0ms;">
             @foreach($Projects as $Project)
-                <div class="swiper-slide swiper-slide-active" style="background-image: url('/img/article/{{$Project->id}}/{{$Project->images->count()}}');  width: 1903px; ">
+                
+                @foreach($Project->images as $image)
+                    @php
+                        if($image->type == 'featured')
+                        { 
+                            $filename = $image->filename; 
+                        }
+                    @endphp
+                @endforeach
+
+                <div class="swiper-slide swiper-slide-active" style="background-image: url('/img/article/{{$Project->id}}/{{$filename}}');  width: 1903px; ">
                     <div class="image-overlay"></div>
                     <div class="container">
                         <div class="banner-text">
@@ -20,6 +30,9 @@
                     </div>
                 </div>
             @endforeach
+
+
+
         </div><!-- End swiper wrapper  -->
 
         <div class="swiper-pagination swiper-pagination-clickable">
